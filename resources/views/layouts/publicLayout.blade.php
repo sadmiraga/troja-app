8d9aa00951e8a6f07777abc283fdef6099aa3682
Sadmir Hasanic <42960898+sadmiraga@users.noreply.github.com>
2025-04-13 00:23:41 +0200

    install clarity on menu

--- resources/views/layouts/publicLayout.blade.php
+++ resources/views/layouts/publicLayout.blade.php
@@ -20,6 +20,13 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <script type="text/javascript">
+        (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+        })(window, document, "clarity", "script", "r378sl1hx9");
+    </script>
 </head>
 
 <body>
